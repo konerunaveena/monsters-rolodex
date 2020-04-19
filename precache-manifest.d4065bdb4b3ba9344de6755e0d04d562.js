663a8cdcfa80ef67fbf1e071827574bc06114093
KoneruNaveena <naveena.koneru536@gmail.com>
2020-04-20 03:25:52 +0530

    Updates

---
+++ precache-manifest.d4065bdb4b3ba9344de6755e0d04d562.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f8ffc4c3c995dbfc85956ee5cface4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "761cd3d1ada2634b5b54",
+    "url": "/monsters-rolodex/static/css/main.5ae4f8ef.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "761cd3d1ada2634b5b54",
+    "url": "/monsters-rolodex/static/js/main.29c9d97e.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
